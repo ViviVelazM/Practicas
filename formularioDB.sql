92a954860ed39cf4507b6bf909bcb52a2d54a897
ViviVelazM <126122036+ViviVelazM@users.noreply.github.com>
2023-09-18 22:52:37 -0600

    Update formularioDB.sql

--- formularioDB.sql
+++ formularioDB.sql
@@ -1,31 +1,15 @@
-/*Script SQL Server (general)*/
-CREATE DATABASE IF NOT EXISTS formularioDB;
-USE formularioDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE IF NOT EXISTS DatosPersonales (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    saludo VARCHAR(4),
-    apellidoPaterno VARCHAR(50),
-    apellidoMaterno VARCHAR(50),
-    nombres VARCHAR(100),
-    sexo INT,
-    email VARCHAR(200),
-    fechaNac DATE,
-    domicilio TEXT
+CREATE TABLE IF NOT EXISTS `DatosPersonales` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `saludo` VARCHAR(4),
+    `apellidoPaterno` VARCHAR(50),
+    `apellidoMaterno` VARCHAR(50),
+    `nombres` VARCHAR(100),
+    `sexo` INT,
+    `email` VARCHAR(200),
+    `fechaNac` VARCHAR(200),
+    `domicilio` VARCHAR(200)
 );
-
-/*Script MySQL (estricto)*/
-/*CREATE DATABASE IF NOT EXISTS formularioDB;
-USE formularioDB;
-
-CREATE TABLE IF NOT EXISTS DatosPersonales (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    saludo VARCHAR(255) NOT NULL,
-    apellidoPaterno VARCHAR(255) NOT NULL,
-    apellidoMaterno VARCHAR(255) NOT NULL,
-    nombres VARCHAR(255) NOT NULL,
-    sexo INT NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    fechaNac DATE NOT NULL,
-    domicilio TEXT NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;*/
