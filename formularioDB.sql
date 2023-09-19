3089a41f3f51a5b4c50e06b1c1b76d615affca32
ViviVelazM <126122036+ViviVelazM@users.noreply.github.com>
2023-09-18 22:56:02 -0600

    Update formularioDB.sql

--- formularioDB.sql
+++ formularioDB.sql
@@ -4,11 +4,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `DatosPersonales` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `saludo` VARCHAR(4),
+    `saludo` VARCHAR(5),
     `apellidoPaterno` VARCHAR(50),
     `apellidoMaterno` VARCHAR(50),
     `nombres` VARCHAR(100),
-    `sexo` INT,
+    `sexo` VARCHAR(3),
     `email` VARCHAR(200),
     `fechaNac` VARCHAR(200),
     `domicilio` VARCHAR(200)
