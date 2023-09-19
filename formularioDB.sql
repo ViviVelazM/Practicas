14e8de7cbb25e6d0fee39843ad8a159a0bcaaa62
ViviVelazM <126122036+ViviVelazM@users.noreply.github.com>
2023-09-19 09:04:26 -0600

    Update formularioDB.sql

--- formularioDB.sql
+++ formularioDB.sql
@@ -1,16 +1,13 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE DATABASE IF NOT EXISTS formularioDB;
+USE formularioDB;
 
-USE `formularioDB`;
-
-CREATE TABLE IF NOT EXISTS `DatosPersonales` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `apellidoPaterno` VARCHAR(50),
-    `apellidoMaterno` VARCHAR(50),
-    `nombres` VARCHAR(100),
-    `sexo` VARCHAR(3),
-    `email` VARCHAR(200),
-    `fechaNac` VARCHAR(200),
-    `domicilio` VARCHAR(200)
+CREATE TABLE IF NOT EXISTS DatosPersonales (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    apellidoPaterno VARCHAR(50),
+    apellidoMaterno VARCHAR(50),
+    nombres VARCHAR(100),
+    sexo INT,
+    email VARCHAR(200),
+    fechaNac DATE,
+    domicilio TEXT
 );
